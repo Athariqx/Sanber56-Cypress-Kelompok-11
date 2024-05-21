f16f0ba8ef8ff4010876f5f6c6ed5e918451b281
aji <ajipangestu0065@gmail.com>
2024-05-22 03:40:11 +0700

    menambahkan komentar di dalam kode program

--- cypress/e2e/choose_update_cart.cy.js
+++ cypress/e2e/choose_update_cart.cy.js
@@ -2,19 +2,28 @@ import Shop from "../support/pageObject/choose_update_POM"
 
 describe("testing choose product", () => {
     it("Visit Home Page", () => {
+
+      //mengunjungi halaman utama
       Shop.visitShop()
+      
+      //memilih produk di etalase
       Shop.chooseProduct()
       cy.get('#option-label-size-143').then(function(e){
         const t = e.text()
         expect(t).to.contains('Size')
         Shop.scrollUp();
+        
+        //memilih 2 produk
         Shop.chooseProductOne();
         Shop.chooseProductTwo();
+        
+        //menampilkan keranjang
         Shop.showCart();
+        
+        //mengupdate kuantitas kedua produk
         Shop.updateCartOne();
         Shop.updateCartTwo();
         }) 
- 
     })
 
 })
